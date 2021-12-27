2d6fbc13730f57329d5da0ee812591d74d95a083
eugdov <eugene.dov@gmail.com>
2021-12-27 22:55:49 +0300

    Решение

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[] myArray = new int[10];
+
+Random rand = new Random();
+for (int x = 0; x < myArray.Length; x++)
+      {
+          myArray[x] = rand.Next(30);
+          Console.Write(myArray[x] + " ");
+      }
+      Console.WriteLine();
+for (int x = 0; x < myArray.Length; x++)
+      {
+          if (myArray[x] % 2 == 0) {
+        Console.Write(myArray[x] + " ");
+        }
+      }
+
+
